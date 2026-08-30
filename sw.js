7407db8113ae54f1e55b4286356169c9eb96ea5e
Ariefw783 <ariefw783@gmail.com>
2026-08-30 12:29:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nama memori penyimpanan (cache)
-const CACHE_NAME = 'dpib-hub-cache-v1';
+const CACHE_NAME = 'dpib-hub-cache-v2';
 
 // Daftar aset penting yang perlu disimpan untuk cadangan offline
 const ASSETS_TO_CACHE = [
